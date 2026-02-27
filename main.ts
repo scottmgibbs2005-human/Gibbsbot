8f6f95a82133ac762fe87f5dc85a226217f47695
scottmgibbs2005-human <scottmgibbs2005@gmail.com>
2026-02-27 10:18:49 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-	
+    servos.P1.setAngle(0)
+})
+input.onButtonPressed(Button.AB, function () {
+    servos.P1.setAngle(90)
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P1.setAngle(180)
 })
